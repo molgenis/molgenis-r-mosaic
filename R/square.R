1c4c2e1b020ac4b59460bf11ab0ed1ccfc6c20c7
connor <c.stroomberg@gmail.com>
2019-02-13 15:06:03 +0100

    add doc example using roxygen cleanup helloworld man file

--- R/square.R
+++ R/square.R
@@ -1,3 +1,9 @@
+#' Square a number
+#'
+#' Takes a numaric value and squares it
+#' @param x a numaric value to be squared
+#' @return the squared input
+#' @export
 square <- function(x) {
   return(x^2)
 }
