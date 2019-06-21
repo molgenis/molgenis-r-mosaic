927ede0eebc1df525debfec056b0a2c908dff699
ljohansson <l.johansson@umcg.nl>
2019-06-21 17:56:04 +0200

    transformed read snp file to select columns by header name

--- R/load_data.R
+++ R/load_data.R
@@ -10,8 +10,18 @@ run.demo <- function() {
   cat("Select snp data","\n")
   snp.file <- file.choose()
   cat("Read snp data","\n")
-  #In the SNP-array file only column 3,4 and 6 are of interest
-  snpm.data <- read.table(snp.file, skip = 9, header = T, sep = "\t")[,c(3,4,6)]
+
+  #In the SNP-array file only columns 'Chr', 'Position and 'B Allele Freq are of interest
+  #First find row with headers (following) row with '[Data]' in column 1.
+  snp.headerlines <- readLines( snp.file, n=20)
+  snp.startdata <- which(grepl(pattern = "Data", x= snp.headerlines))
+  #The column headers are in the following line
+  snp.colnamesrow <- snp.startdata + 1
+  chrom <- which(strsplit(snp.headerlines, "\t")[[snp.colnamesrow]]=="Chr")
+  chrompos <- which(strsplit(snp.headerlines, "\t")[[snp.colnamesrow]]=="Position" )
+  BAF <- which(strsplit(snp.headerlines, "\t")[[snp.colnamesrow]]=="B Allele Freq" )
+  snpm.data <- read.table(snp.file, skip = 9, header = T, sep = "\t")[,c(chrom,chrompos,BAF)]
+
 
   cat("Select snp data","\n")
   deviations.file <- file.choose()
