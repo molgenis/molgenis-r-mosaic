0a043c8c89f104c9e1c4d7697a89d02f00610cbe
connoratrug <c.stroomberg@rug.nl>
2020-01-03 16:02:04 +0100

    task: Cleanup run.demo function
    - Pass experiment number, gender, and output path as function agruments with defaults
    - Update function doc
    - Fix style, use tidyverse style convention

--- R/load_data.R
+++ R/load_data.R
@@ -1,36 +1,38 @@
-#' Run demo using expr number 1 and male = gender
+#' Runner script to try out MosaicCalculator
+#' 
+#' Run demo defaults to using using experiment_number 1, gender "Male" and results_path "./mosaic-result.pdf"
 #' Asks for file to be selected
+#'
+#' @param experiment_number number used for tracking experiment.
+#' @param gender string one of (Male, Female or Unknown).
+#' @param results_path string path and file name of pdf file contaiing results
 #' @export
 #'
-run.demo <- function() {
-  # Demo consts
-  exp.nr <- 1         ##COMMENT: GIVE AS INPUT PARAMETER
-  gender <- "Male"    ##COMMENT: GIVE AS INPUT PARAMETER
+run.demo <- function(experiment_number=1, gender="Male", results_path="./mosaic-result.pdf") {
   # Run demo script
   cat("Select snp data","\n")
-  snp.file <- file.choose()
+  snp_file <- file.choose()
   cat("Read snp data","\n")
 
   #In the SNP-array file only columns 'Chr', 'Position and 'B Allele Freq are of interest
   #First find row with headers (following) row with '[Data]' in column 1.
-  snp.headerlines <- readLines( snp.file, n=20)
-  snp.startdata <- which(grepl(pattern = "Data", x= snp.headerlines))
+  snp_headerlines <- readLines( snp_file, n=20)
+  snp_startdata <- which(grepl(pattern = "Data", x= snp_headerlines))
   #The column headers are in the following line
-  snp.colnamesrow <- snp.startdata + 1
-  chrom <- which(strsplit(snp.headerlines, "\t")[[snp.colnamesrow]]=="Chr")
-  chrompos <- which(strsplit(snp.headerlines, "\t")[[snp.colnamesrow]]=="Position" )
-  BAF <- which(strsplit(snp.headerlines, "\t")[[snp.colnamesrow]]=="B Allele Freq" )
-  snpm.data <- read.table(snp.file, skip = 9, header = T, sep = "\t")[,c(chrom,chrompos,BAF)]
-
+  snp_colnames_row <- snp_startdata + 1
+  chrom <- which(strsplit(snp_headerlines, "\t")[[snp_colnames_row]]=="Chr")
+  chrom_pos <- which(strsplit(snp_headerlines, "\t")[[snp_colnames_row]]=="Position" )
+  BAF <- which(strsplit(snp_headerlines, "\t")[[snp_colnames_row]]=="B Allele Freq" )
+  snpm_data <- read.table(snp_file, skip = 9, header = T, sep = "\t")[,c(chrom, chrom_pos, BAF)]
 
   cat("Select CNV data","\n")
-  deviations.file <- file.choose()
+  deviations_file <- file.choose()
+  
   cat("Read events","\n")
-  deviations <- read.table(deviations.file, header = T, sep = "\t")
+  deviations <- read.table(deviations_file, header = T, sep = "\t")
 
   cat("Run analysis","\n")
-  events.filter <- MosaicCalculator(exp.nr, gender, snpm.data, deviations, "./mosaic-result.pdf")  ##COMMENT CAN THIS BE CHANGES TO NAME FILE AND SELECT LOCATION?
-  print(events.filter)
+  events.filter <- MosaicCalculator(experiment_number, gender, snpm_data, deviations, results_path)
 
   cat("Done","\n")
 }
